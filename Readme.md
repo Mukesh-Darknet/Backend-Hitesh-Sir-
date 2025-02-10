cd36d9a5b91bec616ba05dd2b42137337762d62a
Mukesh Yadav M2 <mukesh@Mukeshs-MacBook-Air.local>
2025-02-11 01:12:38 +0530

    dataabse connection

--- Readme.md
+++ Readme.md
@@ -1,3 +1,7 @@
 # LEarning Backend with Hitesh sir
+to push o git
+-- git add.
+---git commit -m "name"
+---git push
 
 nodemon -- it restart the server automaticaly
---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import {DB_NAME} from '../constant.js'
+
+
+const connectDB = async () =>{
+   try{
+   const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+   console.log(`\n Mongodb is coonected !!! ${connectionInstance.connection.host}`); 
+   
+
+   }catch(error){
+    console.log("MongoDb connection error" , error);
+    
+
+   }
+}
+
+export default connectDB;
